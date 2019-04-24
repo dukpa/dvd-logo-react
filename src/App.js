ed9404c0a8c0650826ed9f73523d438f5cb4b21a
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2019-04-23 23:58:57 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../DVD_logo.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MAX_SPEED = 5;
+const MIN_SPEED = 1;
+const randomBetween = (min,max) => Math.floor(Math.random()*(max-min+1)+min);
+const getValueFromPx = (px) => Number(px.split('px')[0]);
+
+class App extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      position: {
+        left: 0,
+        top: 0,
+        right: undefined,
+        bottom: undefined,
+        atEdge: true
+      },
+      velocity: {
+        left: 0,
+        top: 0
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    requestAnimationFrame(_ => {
+      this.move();
+      this.animate();
+    });
+  }
+
+  move() {
+    const state = this.state;
+    const position = this.determineCurrentPosition();
+    const velocity = position.atEdge ? 
+      this.determineNewVelocity(position) : 
+      state.velocity;
+
+    position.left += velocity.left;
+    position.top += velocity.top;
+
+    this.setState({
+      ...state,
+      position,
+      velocity
+    });
+  }
+
+  determineCurrentPosition() {
+    const logo = document.getElementById('Logo');
+    let {right, left, bottom, top} = getComputedStyle(logo);
+
+    const position = {right, left, bottom, top};
+    for (let key in position) {
+      position[key] = getValueFromPx(position[key]);
+      if (position[key] < 1) {
+        position.atEdge = true;
+      }
+    }
+
+    position.atEdge = !!position.atEdge; //make sure is boolean
+
+    return position;
+  }
+
+  determineNewVelocity(position) {
+    const velocity = {};
+    const oldVelocity = this.state.velocity;
+
+    if (position.left < 1) {
+      velocity.left = randomBetween(MIN_SPEED, MAX_SPEED);
+    } else if (position.right < 1) {
+      velocity.left = randomBetween(-MAX_SPEED, -MIN_SPEED);
+    } else {
+      velocity.left = oldVelocity.left;
+    }
+
+    if (position.top < 1) {
+      velocity.top = randomBetween(MIN_SPEED, MAX_SPEED);
+    } else if (position.bottom < 1) {
+      velocity.top = randomBetween(-MAX_SPEED, -MIN_SPEED);
+    } else {
+      velocity.top = oldVelocity.top;
+    }
+
+    return velocity;
+  }
+
+  render() {
+    const {state} = this;
+    return (
+      <div id="App">
+        <div id="Logo" style={{
+          left: state.position.left,
+          top: state.position.top
+        }}></div>
+      </div>
+    );
+  }
 }
 
 export default App;

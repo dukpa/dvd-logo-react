7aa39f4396389bb31e61d34d395579b85efb9bef
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2019-04-24 00:18:46 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import '../DVD_logo.svg'
+import DvdLogo from './DvdLogo';
 
 const App = (props) => (
   <div id="App">
     <div id="Logo" style={{
       left: props.left,
       top: props.top
-    }}></div>
+    }}>
+      <DvdLogo />
+    </div>
   </div>
 )
 
